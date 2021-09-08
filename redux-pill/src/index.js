6861a1ad6e3125c038653c7ab13023dddd1ce684
Marc <marcsr03@gmail.com>
2021-09-08 11:01:26 +0200

    .

--- redux-pill/src/index.js
+++ redux-pill/src/index.js
@@ -7,6 +7,7 @@ import App from "./App";
 // import { store } from "./app/store";
 // import { Provider } from "react-redux";
 import * as serviceWorker from "./serviceWorker";
+import { createStore } from "redux";
 
 ReactDOM.render(
   // <Provider
